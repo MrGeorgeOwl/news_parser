7decafd3a8d32e8dc614c6a754863a8a53ef6dc6
George Parhomenko <per4ik35@gmail.com>
2020-05-06 15:03:12 +0300

    Change views and urls of news_api

--- news_parser/news_api/urls.py
+++ news_parser/news_api/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path, include
-from rest_framework import routers
-from . import views
+from django.urls import path
+from news_api import views
 
-router = routers.DefaultRouter()
-router.register(r'news', views.ArticleViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('articles/', views.article_list),
+    path('articles/<int:pk>', views.article_detail),
 ]
