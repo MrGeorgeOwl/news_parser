a5fb3cfb48683fbc740648f61269db8dc1b44543
George Parhomenko <parhomenkogeorge@gmail.com>
2020-05-22 13:52:44 +0300

    Delete line

--- news_parser/news_api/models.py
+++ news_parser/news_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Article(models.Model):
     title = models.CharField(max_length=300, default=None)
     author = models.CharField(max_length=50, default=None)
