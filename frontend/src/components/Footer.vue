3821476492a6156e6f45f90868113d7cb483e6bc
George Parhomenko <parhomenkogeorge@gmail.com>
2020-05-22 13:57:12 +0300

    Create components for Header and Footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="footer">
+        <b-container>
+            <b-row>
+                <b-col>
+                    <p>Copyright © 2020, Parhomenko Heorhi.All Rights Reserved.</p>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<style scoped>
+    .footer {
+    width: 100%;
+    height: 3rem;
+    padding: 10px;
+    background-color: #4fc08d;
+    margin-bottom: 0px;
+}
+
+p {
+    font-size: 1rem;
+    color: #FFF;
+    margin-bottom: 0px;
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="header">
+        <b-container>
+            <b-row align-v="center">
+                <b-col>
+                    <p>News Parser</p>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<style scoped>
+.header {
+    width: 100%;
+    height: 3.3rem;
+    padding: 10px;
+    background-color: #4fc08d;
+    margin-bottom: 20px;
+}
+
+p {
+    font-size: 1.5rem;
+    color: #FFF;
+}
+</style>
