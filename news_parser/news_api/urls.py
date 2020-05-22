71399c9b5e0b21030217734a4dde44d3d48adfa7
George Parhomenko <parhomenkogeorge@gmail.com>
2020-05-22 13:53:31 +0300

    Create urls for new api requests

--- news_parser/news_api/urls.py
+++ news_parser/news_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from news_api import views
 
 urlpatterns = [
-    path('articles/', views.article_list),
-    path('articles/<int:pk>', views.article_detail),
+    path(r'articles/', views.article_list),
+    path('articles/<int:pk>/', views.article_detail),
 ]
