bb4cdf081ab8ca55c6c40915115a52fc53e637d3
George Parhomenko <parhomenkogeorge@gmail.com>
2020-05-22 13:54:04 +0300

    Add corsheaders for cross site connection

--- news_parser/news_parser/settings.py
+++ news_parser/news_parser/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8081",
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'news_parser.urls'
 
